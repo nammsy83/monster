f2a3c7e0940890238c436c4c7878bb35102ba72e
Namrata Subhedar <nammsy@gmail.com>
2019-07-24 12:20:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import {Searchbox} from './components/searchbox/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchfield: ""
+    };
+    this.handleclick2 = this.handleclick1.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  handleclick1 (){
+      console.log('button1 clicked');
+  }
+
+  handleclick3 = ()=> {
+    console.log('button 3 clicked');
+  }
+    render(){
+      const { monsters, searchfield } = this.state;
+      const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchfield.toLowerCase()))
+
+      return(
+      
+        <div className="App">
+          <button onClick={this.handleclick1()}>click1</button>
+          <button onClick={this.handleclick1}>click2</button>
+          <button onClick={this.handleclick2}>click3</button>
+          <button onClick={this.handleclick3}>click4</button>
+
+            <Searchbox 
+            placeholder="Search monster" 
+            handleChange={e => this.setState({searchfield: e.target.value})}
+            />
+            <CardList monsters={filteredMonsters}/>
+        </div>
+      )
+    }
 }
 
 export default App;
