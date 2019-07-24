ccb6041fc55dd3dc8527fe4fd21263ec3961f6b6
Namrata Subhedar <nammsy@gmail.com>
2019-07-24 12:26:34 +0530

    Updates

---
+++ precache-manifest.f9226cdf0af5d067d711f56e9c42da73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836f0bf4d43f49633dcd9663c46965f7",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "428f5e7821751f34e7fd",
+    "url": "/monster/static/css/main.bbff778f.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monster/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "428f5e7821751f34e7fd",
+    "url": "/monster/static/js/main.dff3ce3c.chunk.js"
+  },
+  {
+    "revision": "24520cef794f93741fed",
+    "url": "/monster/static/js/runtime~main.488aa6c8.js"
+  }
+]);
